4b48afe893be33874f1992a12974164a2cbe70c0
Ahmed Burak <burak.hamamci@dscience.tech>
2024-05-30 22:45:19 +0300

    AdminController sayfası oluşturuldu.Lazım olucak classlar eklendi.

---
+++ KurumsalWeb/Models/Yorum.cs
@@ -0,0 +1,27 @@
+﻿using KurumsalWeb.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace KurumsalWeb.Models
+{
+    //yorum veritabanı oluşturma
+    [Table("Yorum")]
+    public class Yorum
+    {
+        public int YorumId { get; set; }
+        [Required, StringLength(50, ErrorMessage = "50 Karakter Olabilir.")]
+        public string AdSoyad { get; set; }
+        public string Eposta { get; set; }
+        [DisplayName("Yorumunuz")]
+        public string İcerik { get; set; }
+        public bool Onay { get; set; }
+
+        public int? BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
